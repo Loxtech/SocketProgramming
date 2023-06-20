8bd4ce880de8f8b94ddbf08f38f330b7d1403c98
Loke Bendtsen <lokebendtsen@gmail.com>
2023-06-20 11:11:21 +0200

    Add project files.

---
+++ SocketClient/SocketClient.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SocketClient
+{
+    internal class SocketClient
+    {
+        public SocketClient()
+        {
+            while (true) StartClient();
+        }
+
+        internal void StartClient()
+        {
+            Console.Write("Message: ");
+            string msg = Console.ReadLine() + " <EOM>";
+            IPAddress iPServerPAddress = IPAddress.Parse("192.168.2.2");
+            IPEndPoint serverEndPoint = new(iPServerPAddress, 22222);
+
+            Socket sender = new(
+                iPServerPAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            sender.Connect(serverEndPoint);
+            Console.WriteLine("Connected to: " + serverEndPoint.ToString());
+
+            byte[] byteArr = Encoding.ASCII.GetBytes(msg);
+            sender.Send(byteArr);
+
+            string returnMsg = GetMessage(sender);
+            Console.WriteLine(returnMsg);
+
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+        private static string GetMessage(Socket socket)
+        {
+            string data = null;
+            byte[] bytes = null;
+
+            while (true)
+            {
+                bytes = new byte[4096];
+                int bytesRec = socket.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                if (data.Contains("<EOM>")) break;
+            }
+
+            return data;
+        }
+    }
+}
---
+++ SocketProgrammingServer/SocketServer.cs
@@ -0,0 +1,80 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketProgrammingServer
+{
+    internal class SocketServer
+    {
+        public SocketServer() 
+        {
+            while (true) StartServer();  
+        }
+        internal void StartServer()
+        {
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName(), System.Net.Sockets.AddressFamily.InterNetwork);
+            int choice = ChooseIpAddress(iPHostEntry.AddressList);
+
+            IPAddress iPAddress = iPHostEntry.AddressList[1];
+            IPEndPoint iPEndPoint = new(iPAddress, 22222);
+            Socket listener = new(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            listener.Bind(iPEndPoint);
+            listener.Listen(10);
+            Console.WriteLine("Server listens on: " + iPEndPoint);
+            
+            while(true) ConnectToClient(listener);
+        }
+        private static void ConnectToClient(Socket listener)
+        {
+            Socket handler = listener.Accept();
+            Console.WriteLine("Connected to: " + handler.RemoteEndPoint.ToString());
+            string data = GetMessage(handler);
+
+            byte[] returnMsg = Encoding.ASCII.GetBytes("Server received msg <EOM>");
+            handler.Send(returnMsg);
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+
+            Console.WriteLine(data);
+        }
+        private static string GetMessage(Socket socket)
+        {
+            string data = null;
+            byte[] bytes = null;
+
+            while (true)
+            {
+                bytes = new byte[4096];
+                int bytesRec = socket.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                if (data.Contains("<EOM>")) break;
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Select IP from ip address array
+        /// </summary>
+        /// <param name="addressList"></param>
+        /// <returns>int</returns>
+        private int ChooseIpAddress(IPAddress[] addressList)
+        {
+            var i = 0;
+            foreach( var item in addressList )
+            {
+                Console.WriteLine($"[{i++}] {item}");
+            }
+          
+            int j;
+            do
+            {
+                Console.WriteLine("Input number of ip address: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out j) || j<0 || j>=addressList.Length);
+            return j;
+        }
+    }
+}
